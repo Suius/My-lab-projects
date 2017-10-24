19214f9d638849cb65682d4cf5479a7c07cfb5c9
Suius <31655841+Suius@users.noreply.github.com>
2017-10-24 15:38:33 +0300

    Reverse_Polsish_Notation

--- main.c
+++ main.c
@@ -1,64 +1,64 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main(int argc, char **argv)
+
+int main()
 {
-    float K, F, C, Temperature;
-    char Letter;
-    scanf("%f", &Temperature);
-    scanf("%c", &Letter);
-    switch (Letter)
+    int stack[1000];
+    int n = 0;
+    printf("Instructions:\n\nEnter numbers then mathematical signs and the equation sign to show the result;\n\nExample: 15 5 * =\n\nEnter the letter 'e' when finished\n\n");
+    while (!feof(stdin))
     {
-       case 'c':
-       case 'C':
-           if (Temperature <= -273.15)
-           {
-               printf("This is impossible");
-           }
-           else
-           {
-               F = Temperature *9/5 + 32;
-               K = Temperature + 273.15;
-               printf ("%.2f F\n%.2f K\n", F, K);
-           }
-           break;
-       case 'F':
-       case 'f':
-           if (Temperature<=-469.27)
-           {
-               printf("This is impossible");
-           }
-           else
-           {
-               C = (Temperature-32)*5/9;
-               K = C + 273.15;
-               printf("%.2f C\n%.2f K\n", C, K);
-           }
-           break;
-       case 'k':
-       case 'K':
-           if (Temperature <= 0)
-           {
-               printf("This is impossible");
-           }
-           else
-           {
-               C = Temperature - 273.15;
-               F = C * 9/5 + 32;
-               printf("%.2f C\n%.2f F\n", C, F);
-           }
-           break;
-       default:
-           if (Temperature <= -273.15)
-           {
-               printf("This is impossible");
-           }
-           else
-           {
-                F = Temperature * 9/5 + 32;
-                K = Temperature + 273.15;
-                printf ("%.2f C\n%.2f F\n%.2f K\n",Temperature , F, K);
-           }
-           break;
+        int symbol = getchar();
+        int x;
+        switch (symbol)
+        {
+        case ' ':
+        case '\n':
+            break;
+
+
+        case '+':
+            stack[n-2]=stack[n-2]+stack[n-1];
+            n--;
+            break;
+
+
+        case '-':
+            stack[n-2] = stack[n-2]-stack[n-1];
+            n--;
+            break;
+
+        case '*':
+            stack[n-2]=stack[n-2]*stack[n-1];
+            n--;
+            break;
+
+        case '/':
+            stack[n-2]=stack[n-2]/stack[n-1];
+            n--;
+            break;
+
+
+        case '=':
+            printf ("Result = %d\n", stack[n-1]);
+            n--;
+            break;
+        case 'e':
+            printf ("\nDone\n");
+            return 0;
+        default:
+            ungetc (symbol, stdin);
+            if (scanf("%d",&x)!=1)
+            {
+                fprintf (stderr, "Can't understand what you typed in\n");
+                return -1;
+            }
+            else
+            {
+                stack[n] = x;
+                n++;
+            }
+        }
     }
+    printf("Result = %d\n", stack[n]);
     return 0;
 }
