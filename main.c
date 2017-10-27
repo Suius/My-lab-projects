b94e05bfbeef71481e01a5f7c2d4c3e32fd35f8c
Suius <31655841+Suius@users.noreply.github.com>
2017-10-27 16:59:43 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,15 @@ int main(int argc, char *argv[])
 {
     int stack[1000];
     int n = 0;
-    printf("Instructions:\n\nEnter numbers then mathematical signs and the equation sign to show the result;\n\nExample: 15 5 * =\n\nEnter the letter 'e' when finished\n\n");
+    int counter_numbers, counter_signs;
+    counter_numbers = 0;
+    counter_signs = 1;
+    printf("Instructions:\n\nEnter numbers then mathematical signs and the equation sign to show the result;\n\nExample: 15 5 + =\n\nEnter the letter 'e' when finished\n\n");
     while (!feof(stdin))
     {
         int symbol = getchar();
-        int thingy;
+        int x;
+
         switch (symbol)
         {
         case ' ':
@@ -19,46 +23,59 @@ int main(int argc, char *argv[])
         case '+':
             stack[n-2]=stack[n-2]+stack[n-1];
             n--;
+            counter_signs++;
             break;
 
 
         case '-':
             stack[n-2] = stack[n-2]-stack[n-1];
             n--;
+            counter_signs++;
             break;
 
         case '*':
             stack[n-2]=stack[n-2]*stack[n-1];
             n--;
+            counter_signs++;
             break;
 
         case '/':
             stack[n-2]=stack[n-2]/stack[n-1];
             n--;
+            counter_signs++;
             break;
 
 
         case '=':
+            if (counter_numbers == counter_signs)
+            {
             printf ("Result = %d\n", stack[n-1]);
             n--;
             break;
+            }
+            else
+            {
+                printf("\nERROR");
+                return -2;
+            }
+
         case 'e':
             printf ("\nDone\n");
             return 0;
+
         default:
             ungetc (symbol, stdin);
             if (scanf("%d",&x)!=1)
             {
-                fprintf (stderr, "Can't understand what you typed in\n");
+                printf ("Can't understand what you typed in\n");
                 return -1;
             }
             else
             {
-                stack[n] = thingy;
+                stack[n] = x;
                 n++;
             }
         }
     }
-    printf("Result = %d\n", stack[n]);
     return 0;
 }
